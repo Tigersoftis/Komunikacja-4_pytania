56745b308dc4d5d074838435914b3273163caf9b
Seba <tigersoft@wp.pl>
2016-02-22 15:45:22 +0100

    Added files via upload

---
+++ komunikacja.z.komputerem.1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+
+{
+	string name;
+	cout << "What is Your name?\n";
+	cin >> name;
+	cout << "Hello " << name << "!" << endl << endl;
+	system ("pause");
+	
+	cout<< "\n How old are You?" << endl;
+	int age;
+	cin >> age;
+	cout << "You are " << age << " years old," << endl; 
+	cout << "It's good age :)" << endl << endl; 
+	system ("pause");
+	
+	cout << "\n Where Are You from?\n";
+	string place;
+	cin >> place;
+	cout << place << " it's a beautiful place! \n \n";
+	system ("pause");
+	
+	cout << "\n What's your eyes color?" << endl;
+	string kolor;
+	cin >> kolor;
+	cout << "\n You have " << kolor << " eyes?" << " It's amazing!!! \n \n";
+	system ("pause");
+	
+	cout << "\n Created by Tigersoft \n \n";
+	
+	system ("pause");
+	
+	return 0;
+}
